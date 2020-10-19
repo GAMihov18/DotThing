afa33e88f7845a1f91a6e319f153e74959f96bf1
RoyalFang <GAMihov18@codingburgas.bg>
2020-10-19 19:05:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ const TBXCoord = document.getElementById('dcx');
 const TBYCoord = document.getElementById('dcy');
 const coordTable = document.getElementById('coordinateTable');
 
+//Makes the dot
 function makeDots(event) {
+  clearTable();
   event.preventDefault();
   let xcoord = (parseInt(TBXCoord.value,10))-2;
   let ycoord = (parseInt(TBYCoord.value,10))-2;
   let div = document.createElement('DIV');
   div.classList = 'dotDiv';
-  div.innerHTML=`<p class="dot" style="top:${ycoord}px; left:${xcoord}px"></p>`;
+  div.innerHTML = `<p class="dot" style="top:${ycoord}px; left:${xcoord}px"></p>`;
   coordTable.appendChild(div);
   console.log(coordTable);
 }
+
+//Clears table
+function clearTable() {
+  coordTable.innerHTML=``;
+}
+
 //Logs
 console.log(TBXCoord);
 console.log(TBYCoord);
